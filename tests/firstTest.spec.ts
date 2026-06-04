74959f9db83894d797c4d5f69f9deb4689f71a93
Mohamedramadan578 <mohamedramadanhegazy.11@gmail.com>
2026-06-05 02:28:32 +0300

    initializing playwright project

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('firt test heroku', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/');
+  await page.getByRole('link', { name: 'Form Authentication' }).click();
+  await expect(page.getByRole('textbox', { name: 'Username' })).toBeVisible();
+  await expect(page.locator('#login')).toContainText('Username');
+  await expect(page.getByRole('button')).toMatchAriaSnapshot(`- button " Login"`);
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
+  await page.getByRole('button', { name: ' Login' }).click();
+  await expect(page.getByText('You logged into a secure area')).toBeVisible();
+  await expect(page.locator('#flash')).toContainText('You logged into a secure area! ×');
+});
