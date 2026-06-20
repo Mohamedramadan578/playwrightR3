e289f3302313889f86afdea8de30cd098bdd125e
Mohamedramadan578 <mohamedramadanhegazy.11@gmail.com>
2026-06-20 03:17:23 +0300

    assiment3

--- tests/actions.spec.ts
+++ tests/actions.spec.ts
@@ -149,4 +149,72 @@ await page.waitForTimeout(5*1000)
 
 
 });
+test('Scenario 06:upload' ,async({page})=>{
 
+    const usernameInputField:Locator =page.getByRole('textbox', {name :"Username"})
+    const passwordInputField:Locator =page.locator('[id="password-input"]')
+    const loginBtn :Locator=page.locator('//*[@id="btn-login"]')
+    const uploadFileSection :Locator=page.locator('[id="link-7"]')
+    const fileUploadBtn :Locator=page.locator('[id="file-input"]')
+
+    const username:string="trainer"
+    const password:string="selenium123"
+//await page.waitForTimeout(3*1000)
+await page .goto("https://nebula-test-lab-lv1.vercel.app/")
+await usernameInputField.fill(username)
+await passwordInputField.pressSequentially(password,{delay:300})
+await loginBtn.click()
+await clickOnElement((uploadFileSection),page)
+
+const fileUpload =await Promise.all([
+page.waitForEvent('filechooser'),
+fileUploadBtn.click()
+]
+)
+await fileUpload[0].setFiles('C:/Users/Mohamed/Desktop/playwrightR3/New Microsoft Word Document.docx')
+await page.waitForTimeout(5*1000)
+});
+
+test('scenario 07:alert' ,async({page})=>{
+    const usernameInputField:Locator =page.getByRole('textbox', {name :"Username"})
+    const passwordInputField:Locator =page.locator('[id="password-input"]')
+    const loginBtn :Locator=page.locator('//*[@id="btn-login"]')
+    const alertSection:Locator=page.locator('[id="link-12"]')
+    const simpleAlert :Locator=page.locator('[id="btn-alert"]')
+    const confirmationAlert :Locator=page.locator('[id="btn-confirm"]')
+    const promptAlert:Locator=page.locator('[id="btn-prompt"]')
+
+    const username:string="trainer"
+    const password:string="selenium123"
+//await page.waitForTimeout(3*1000)
+await page .goto("https://nebula-test-lab-lv1.vercel.app/")
+await usernameInputField.fill(username)
+await passwordInputField.pressSequentially(password,{delay:300})
+await loginBtn.click()
+await clickOnElement((alertSection),page)
+//page.on('dialog',async(alert)=>{
+//const alertMessage=alert.message();
+  //  expect(alertMessage).toEqual('Simple alert')
+   //await alert.accept();
+//})
+//await simpleAlert.click()
+
+//page.on('dialog',async(alert)=>{
+//const alertMessage=alert.message();
+  // expect(alertMessage).toEqual('Are you sure?')
+    
+  // await alert.accept();
+//})
+//await confirmationAlert.click()
+const promptMsg="Hey this is mohamed"
+page.on('dialog',async(alert)=>{
+const alertMessage=alert.message();
+   expect(alertMessage).toEqual('Type anything:')
+   await alert.accept(promptMsg);
+})
+
+await promptAlert.click()
+await expect(page.locator('[id="prompt-out"]')).toContainText(promptMsg)
+await page.waitForTimeout(5*1000)
+
+});
