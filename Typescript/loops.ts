dc9182c19d1b577604a1587c926be3e2fa352ca8
Mohamedramadan578 <mohamedramadanhegazy.11@gmail.com>
2026-06-27 10:03:17 +0300

    task 4

---
+++ Typescript/loops.ts
@@ -0,0 +1,32 @@
+for (let i=1;i<=5;i++){
+
+console.log(`Hello customer number ${i}`)
+
+}
+let targetNumber=100;
+for(let i=1;i<=targetNumber;i++){
+    if(i==7){
+        console.log(`Hello customer number 7 is found`)
+    }}
+    let numbers=[1,2,3,4,5,6,7,8,9,10]
+    for(const number of numbers){
+        //if(number==7){  console.log(`Hello customer number 7 is found`)}
+        console.log(`${number}`)
+
+    }
+    let i=1;
+    while(i<=5){
+        if(i==4){
+        console.log(`Hello customer number 4 is found`)}
+i++
+    }
+
+    do{
+
+        console.log(`Hello customer number 4 is found do while`)
+        i++
+    }
+        while(i<=5);
+            
+        
+    
